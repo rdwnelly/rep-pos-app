852b6a5dd213a4d48b3bac9827c3b40bf03ae7c1
Ridwan Elly <ridwanelly123@gmail.com>
2026-06-11 22:01:34 +0900

    Initial commit: Migrasi Aplikasi Kasir ke Next.js & Firebase

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./index.html",
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./index.html.backup",
         "./src/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./screens/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
         "./utils/**/*.{js,ts,jsx,tsx}",
         "./*.{js,ts,jsx,tsx}"
